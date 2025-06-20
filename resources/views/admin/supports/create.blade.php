ccfb24005074cc49df057021b14ccb466b7bcfa4
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-06-20 11:00:49 -0300

    Rotas e Controller criados

---
+++ resources/views/admin/supports/create.blade.php
@@ -0,0 +1,8 @@
+<h1>Novo Sup {{$support->id}}</h1>
+
+<form action="{{route('supports.store')}}" method="post">
+    @csrf
+    <input type="text" placeholder="Assunto" name="subject" value="">
+    <textarea name="body" cols="30" rows="5" placeholder="descricao" ></textarea>
+    <button type="submit">Enviar</button>
+</form>
---
+++ resources/views/admin/supports/edit.blade.php
@@ -0,0 +1,9 @@
+<h1>Novo Sup {{$support->id}}</h1>
+
+<form action="{{route('supports.update', $support->id)}}" method="post">
+    @csrf
+    <input type="text" value="PUT" name="_method" hidden>
+    <input type="text" placeholder="Assunto" name="subject" value="{{$support->subject}}" required>
+    <textarea name="body" cols="30" rows="5" placeholder="descricao" >{{$support->body}}</textarea>
+    <button type="submit">Enviar</button>
+</form>
