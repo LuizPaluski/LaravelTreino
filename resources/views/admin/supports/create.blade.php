121d40adecd351e08fee18f9f8e1a8ade6f52ea9
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-06-23 08:41:21 -0300

    Atualizacoes

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,14 @@
 <h1>Novo Suporte</h1>
 
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        {{$error}}
+    @endforeach
+@endif
+
 <form action="{{route('supports.store')}}" method="post">
     @csrf
-    <input type="text" placeholder="Assunto" name="subject" value="">
-    <textarea name="body" cols="30" rows="5" placeholder="descricao" ></textarea>
+    <input type="text" placeholder="Assunto" name="subject" value="{{old('subject')}}">
+    <textarea name="body" cols="30" rows="5" placeholder="descricao" >{{old('body')}}</textarea>
     <button type="submit">Enviar</button>
 </form>
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,5 +1,11 @@
 <h1>Id do suporte {{$support->id}}</h1>
 
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        {{$error}}
+    @endforeach
+@endif
+
 <form action="{{route('supports.update', $support->id)}}" method="post">
     @csrf
     <input type="text" value="PUT" name="_method" hidden>
